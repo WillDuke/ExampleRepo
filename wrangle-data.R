bf3beda8c4fa5bd4e7f1e5db89e51c560b512fa0
WillDuke <Will@WillIams-MacBook-Pro-3.local>
2018-08-09 15:35:57 -0400

    comment wrangle.R

--- wrangle-data.R
+++ wrangle-data.R
@@ -1,3 +1,4 @@
+#use the tidyverse to format data and save as an .rda
 library(tidyverse)
 murders <- read.csv("raw_data/murders.csv")
 murders <- murders %>% mutate(region = factor(region), rate = total/population*10^5)
