7f792348568f5a89f6161bf8a69c30ddc0650d2b
WillDuke <Will@WillIams-MacBook-Pro-3.local>
2018-08-09 15:29:35 -0400

    adding example files

---
+++ wrangle-data.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+murders <- read.csv("raw_data/murders.csv")
+murders <- murders %>% mutate(region = factor(region), rate = total/population*10^5)
+save(murders, file = "R_data/murders.rda")
