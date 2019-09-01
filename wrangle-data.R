c4a11dee510cc210709c1f06e7dc34b22d77182b
Will Duke <willduke.wd@gmail.com>
2019-09-01 14:27:57 -0400

    added plot

--- wrangle-data.R
+++ wrangle-data.R
@@ -4,5 +4,5 @@ library(tidyverse)
 murders <- read.csv("raw_data/murders.csv")
 murders <- murders %>% mutate(region = factor(region), rate = total/population*10^5)
 save(murders, file = "R_data/murders.rda")
-murderPlot <- murders %>% ggplot(aes(region, population)) + geom_bar(stat = "identity")
-murderPlot
+murders %>% ggplot(aes(region, population)) + geom_bar(stat = "identity")
+ggsave("figures/PopByRegion.png")
