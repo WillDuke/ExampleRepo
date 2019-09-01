5f0f1bc97d06b3f1e9b2314b384bbfe776338961
Will Duke <willduke.wd@gmail.com>
2019-09-01 14:22:53 -0400

    added simple bar chart

--- wrangle-data.R
+++ wrangle-data.R
@@ -1,5 +1,8 @@
 #use the tidyverse to format data and save as an .rda
+install.packages("tidyverse")
 library(tidyverse)
 murders <- read.csv("raw_data/murders.csv")
 murders <- murders %>% mutate(region = factor(region), rate = total/population*10^5)
 save(murders, file = "R_data/murders.rda")
+murderPlot <- murders %>% ggplot(aes(region, population)) + geom_bar(stat = "identity")
+murderPlot
